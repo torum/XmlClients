b955ee870c23b5cd66cc4dd3b8752ce01a7322a3
torum <37834607+torum@users.noreply.github.com>
2023-02-16 10:32:20 +0900

    a few updates

--- BlogWrite.Core/Models/NodeEntry.cs
+++ BlogWrite.Core/Models/NodeEntry.cs
@@ -73,7 +73,7 @@ public abstract class EntryItem : Node
     {
         get
         {
-            int maxLength = 220;
+            var maxLength = 220;
             if (string.IsNullOrEmpty(_summary)) return _summary;
             return _summary.Length <= maxLength ? _summary : _summary.Substring(0, maxLength) + " ...";
         }
@@ -83,7 +83,7 @@ public abstract class EntryItem : Node
     {
         get
         {
-            int maxLength = 100;
+            var maxLength = 100;
             if (string.IsNullOrEmpty(_summary)) return _summary;
             return _summary.Length <= maxLength ? _summary : _summary.Substring(0, maxLength) + " ...";
         }
@@ -344,7 +344,7 @@ public abstract class EntryItem : Node
 
     public static string TimeAgo(DateTime dateTime)
     {
-        var result = string.Empty;
+        string result;
         var timeSpan = DateTime.Now.Subtract(dateTime);
 
         if (timeSpan <= TimeSpan.FromSeconds(60))
@@ -478,7 +478,8 @@ public class FeedEntryItem : EntryItem
             }
             else
             {
-                return _source + " via " + _feedTitle;
+                //return _source + " via " + _feedTitle;
+                return _source + " " + "FeedEntryItem_Publisher_Via".GetLocalized() + " " + _feedTitle;
             }
         }
         set
@@ -554,12 +555,12 @@ public class FeedEntryItem : EntryItem
 public class EditEntryItem : EntryItem
 {
     // Icon Path
-    private static string _esNew = "M13,9V3.5L18.5,9M6,2C4.89,2 4,2.89 4,4V20A2,2 0 0,0 6,22H18A2,2 0 0,0 20,20V8L14,2H6Z";
-    private static string _esDraft = "M6,2C4.89,2 4,2.9 4,4V20A2,2 0 0,0 6,22H18A2,2 0 0,0 20,20V8L14,2H6M13,3.5L18.5,9H13V3.5M12,11A3,3 0 0,1 15,14V15H16V19H8V15H9V14C9,12.36 10.34,11 12,11M12,13A1,1 0 0,0 11,14V15H13V14C13,13.47 12.55,13 12,13Z";
-    private static string _esNormal = "M13,9H18.5L13,3.5V9M6,2H14L20,8V20A2,2 0 0,1 18,22H6C4.89,22 4,21.1 4,20V4C4,2.89 4.89,2 6,2M15,18V16H6V18H15M18,14V12H6V14H18Z";
-    private static string _esUpdating = "M14,2H6A2,2 0 0,0 4,4V20A2,2 0 0,0 6,22H18A2,2 0 0,0 20,20V8L14,2M12,18C9.95,18 8.19,16.76 7.42,15H9.13C9.76,15.9 10.81,16.5 12,16.5A3.5,3.5 0 0,0 15.5,13A3.5,3.5 0 0,0 12,9.5C10.65,9.5 9.5,10.28 8.9,11.4L10.5,13H6.5V9L7.8,10.3C8.69,8.92 10.23,8 12,8A5,5 0 0,1 17,13A5,5 0 0,1 12,18Z";
-    private static string _esQueueUpdate = "M14,2H6C4.89,2 4,2.89 4,4V20A2,2 0 0,0 6,22H18A2,2 0 0,0 20,20V8L14,2M12.54,19.37V17.37H8.54V15.38H12.54V13.38L15.54,16.38L12.54,19.37M13,9V3.5L18.5,9H13Z";
-    private static string _esQueuePost = "M13,9H18.5L13,3.5V9M6,2H14L20,8V20A2,2 0 0,1 18,22H6C4.89,22 4,21.1 4,20V4C4,2.89 4.89,2 6,2M11,15V12H9V15H6V17H9V20H11V17H14V15H11Z";
+    private static readonly string _esNew = "M13,9V3.5L18.5,9M6,2C4.89,2 4,2.89 4,4V20A2,2 0 0,0 6,22H18A2,2 0 0,0 20,20V8L14,2H6Z";
+    private static readonly string _esDraft = "M6,2C4.89,2 4,2.9 4,4V20A2,2 0 0,0 6,22H18A2,2 0 0,0 20,20V8L14,2H6M13,3.5L18.5,9H13V3.5M12,11A3,3 0 0,1 15,14V15H16V19H8V15H9V14C9,12.36 10.34,11 12,11M12,13A1,1 0 0,0 11,14V15H13V14C13,13.47 12.55,13 12,13Z";
+    private static readonly string _esNormal = "M13,9H18.5L13,3.5V9M6,2H14L20,8V20A2,2 0 0,1 18,22H6C4.89,22 4,21.1 4,20V4C4,2.89 4.89,2 6,2M15,18V16H6V18H15M18,14V12H6V14H18Z";
+    private static readonly string _esUpdating = "M14,2H6A2,2 0 0,0 4,4V20A2,2 0 0,0 6,22H18A2,2 0 0,0 20,20V8L14,2M12,18C9.95,18 8.19,16.76 7.42,15H9.13C9.76,15.9 10.81,16.5 12,16.5A3.5,3.5 0 0,0 15.5,13A3.5,3.5 0 0,0 12,9.5C10.65,9.5 9.5,10.28 8.9,11.4L10.5,13H6.5V9L7.8,10.3C8.69,8.92 10.23,8 12,8A5,5 0 0,1 17,13A5,5 0 0,1 12,18Z";
+    private static readonly string _esQueueUpdate = "M14,2H6C4.89,2 4,2.89 4,4V20A2,2 0 0,0 6,22H18A2,2 0 0,0 20,20V8L14,2M12.54,19.37V17.37H8.54V15.38H12.54V13.38L15.54,16.38L12.54,19.37M13,9V3.5L18.5,9H13Z";
+    private static readonly string _esQueuePost = "M13,9H18.5L13,3.5V9M6,2H14L20,8V20A2,2 0 0,1 18,22H6C4.89,22 4,21.1 4,20V4C4,2.89 4.89,2 6,2M11,15V12H9V15H6V17H9V20H11V17H14V15H11Z";
 
     // IsDraft flag. AtomPub and XML-PRC WP. MP doesn't have this?
     public bool IsDraft { get; set; }
@@ -578,10 +579,7 @@ public class EditEntryItem : EntryItem
     private EditStatus _es;
     public EditStatus Status
     {
-        get
-        {
-            return _es;
-        }
+        get => _es;
         set
         {
             if (_es == value)
@@ -654,8 +652,8 @@ public class AtomEntry : EntryFull
 
     public XmlDocument AsXmlDoc()
     {
-        XmlDocument xdoc = new XmlDocument();
-        XmlDeclaration xdec = xdoc.CreateXmlDeclaration("1.0", "UTF-8", null);
+        var xdoc = new XmlDocument();
+        var xdec = xdoc.CreateXmlDeclaration("1.0", "UTF-8", null);
         xdoc.AppendChild(xdec);
         /*
 			<entry xmlns="http://www.w3.org/2005/Atom" xmlns:app="http://www.w3.org/2007/app">
@@ -679,17 +677,17 @@ public class AtomEntry : EntryFull
 			  </entry>
          */
 
-        XmlNamespaceManager atomNsMgr = new XmlNamespaceManager(xdoc.NameTable);
+        var atomNsMgr = new XmlNamespaceManager(xdoc.NameTable);
         atomNsMgr.AddNamespace("atom", "http://www.w3.org/2005/Atom");
         atomNsMgr.AddNamespace("app", "http://www.w3.org/2007/app");
 
-        XmlElement rootNode = xdoc.CreateElement(string.Empty, "entry", "http://www.w3.org/2005/Atom");
+        var rootNode = xdoc.CreateElement(string.Empty, "entry", "http://www.w3.org/2005/Atom");
         //rootNode.SetAttribute("xmlns", "http://www.w3.org/2005/Atom");
         rootNode.SetAttribute("xmlns:app", "http://www.w3.org/2007/app");
         xdoc.AppendChild(rootNode);
 
-        XmlElement idNode = xdoc.CreateElement(string.Empty, "id", "http://www.w3.org/2005/Atom");
-        XmlText idTextNode = xdoc.CreateTextNode(this.EntryId);
+        var idNode = xdoc.CreateElement(string.Empty, "id", "http://www.w3.org/2005/Atom");
+        var idTextNode = xdoc.CreateTextNode(this.EntryId);
         idNode.AppendChild(idTextNode);
         rootNode.AppendChild(idNode);
 
@@ -703,21 +701,21 @@ public class AtomEntry : EntryFull
 
         //category
 
-        XmlElement titleNode = xdoc.CreateElement(string.Empty, "title", "http://www.w3.org/2005/Atom");
-        XmlText titleTextNode = xdoc.CreateTextNode(this.Name);
+        var titleNode = xdoc.CreateElement(string.Empty, "title", "http://www.w3.org/2005/Atom");
+        var titleTextNode = xdoc.CreateTextNode(this.Name);
         titleNode.AppendChild(titleTextNode);
         rootNode.AppendChild(titleNode);
 
-        XmlElement contentNode = xdoc.CreateElement(string.Empty, "content", "http://www.w3.org/2005/Atom");
+        var contentNode = xdoc.CreateElement(string.Empty, "content", "http://www.w3.org/2005/Atom");
         contentNode.SetAttribute("type", this.ContentTypeString);
-        XmlText contentTextNode = xdoc.CreateTextNode(this.Content);
+        var contentTextNode = xdoc.CreateTextNode(this.Content);
         contentNode.AppendChild(contentTextNode);
         rootNode.AppendChild(contentNode);
 
-        XmlElement controlNode = xdoc.CreateElement("app", "control", "http://www.w3.org/2007/app");
-        XmlElement draftNode = xdoc.CreateElement("app", "draft", "http://www.w3.org/2007/app");
-        string yesOrNo = this.IsDraft ? "yes" : "no";
-        XmlText draftTextNode = xdoc.CreateTextNode(yesOrNo);
+        var controlNode = xdoc.CreateElement("app", "control", "http://www.w3.org/2007/app");
+        var draftNode = xdoc.CreateElement("app", "draft", "http://www.w3.org/2007/app");
+        var yesOrNo = this.IsDraft ? "yes" : "no";
+        var draftTextNode = xdoc.CreateTextNode(yesOrNo);
         draftNode.AppendChild(draftTextNode);
         controlNode.AppendChild(draftNode);
         rootNode.AppendChild(controlNode);
@@ -729,28 +727,24 @@ public class AtomEntry : EntryFull
     public string AsUTF8Xml()
     {
         var sb = new StringBuilder();
-        using (var stringWriter = new StringWriterWithEncoding(sb, Encoding.UTF8))
-        using (var xmlTextWriter = XmlWriter.Create(stringWriter))
-        {
-            XmlDocument xdoc = AsXmlDoc();
+        using var stringWriter = new StringWriterWithEncoding(sb, Encoding.UTF8);
+        using var xmlTextWriter = XmlWriter.Create(stringWriter);
+        XmlDocument xdoc = AsXmlDoc();
 
-            xdoc.WriteTo(xmlTextWriter);
-            xmlTextWriter.Flush();
-            return stringWriter.GetStringBuilder().ToString();
-        }
+        xdoc.WriteTo(xmlTextWriter);
+        xmlTextWriter.Flush();
+        return stringWriter.GetStringBuilder().ToString();
     }
 
     public string AsUTF16Xml()
     {
-        using (var stringWriter = new System.IO.StringWriter())
-        using (var xmlTextWriter = XmlWriter.Create(stringWriter))
-        {
-            XmlDocument xdoc = AsXmlDoc();
+        using var stringWriter = new System.IO.StringWriter();
+        using var xmlTextWriter = XmlWriter.Create(stringWriter);
+        var xdoc = AsXmlDoc();
 
-            xdoc.WriteTo(xmlTextWriter);
-            xmlTextWriter.Flush();
-            return stringWriter.GetStringBuilder().ToString();
-        }
+        xdoc.WriteTo(xmlTextWriter);
+        xmlTextWriter.Flush();
+        return stringWriter.GetStringBuilder().ToString();
     }
 
 }
@@ -762,10 +756,7 @@ public class AtomEntryHatena : AtomEntry
 
     public string FormattedContent
     {
-        get
-        {
-            return _formattedContent;
-        }
+        get => _formattedContent;
         set
         {
             if (value == _formattedContent)
@@ -813,11 +804,5 @@ public class StringWriterWithEncoding : StringWriter
         this.m_Encoding = encoding;
     }
     private readonly Encoding m_Encoding;
-    public override Encoding Encoding
-    {
-        get
-        {
-            return this.m_Encoding;
-        }
-    }
+    public override Encoding Encoding => this.m_Encoding;
 }
